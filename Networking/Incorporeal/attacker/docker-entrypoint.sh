30f2119a52d18dde44855d344b589f0eb5cd10b8
spenceradolph <spenceradolph@ymail.com>
2021-10-22 11:12:27 -0500

    working concepts

---
+++ Networking/Incorporeal/attacker/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Misconfigurations go here
+ifconfig eth1 171.16.0.1
+
+# self-deleting script
+rm -- "$0"
+
+# make the entrypoint a pass through that then runs the docker command
+# https://stackoverflow.com/questions/39082768/what-does-set-e-and-exec-do-for-docker-entrypoint-scripts#:~:text=exec%20%22%24%40%22%20is%20typically%20used,to%20the%20command%20line%20arguments.
+exec "$@"
